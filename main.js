35831b133056f97af550ca1415bf3551ba7be62c
majksa <info@majksa.cz>
2021-11-25 19:46:05 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow.loadURL('https://app.asana.com/')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
